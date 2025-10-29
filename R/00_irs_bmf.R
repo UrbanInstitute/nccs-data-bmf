76ca1f2ec5d27813e1d8dd45ef3ba9b714524e9f
Thiyaghessan <thiyaghessan@gmail.com>
2025-10-29 17:47:13 -0400

    Rename objects for readability, source required helper scripts at the start, add preprocessing steps for: subsection code, classification code and ruling date. and create dimension table for classification code in tidy format.

--- R/00_irs_bmf.R
+++ R/00_irs_bmf.R
@@ -1,9 +1,16 @@
 # Script to process raw bmf from the IRS
 
+# Library
+library(data.table)
+
 # Helper scripts
 source(here::here("R", "config.R"))
 source(here::here("R", "ein.R"))
-
+source(here::here("R", "organization_name.R"))
+source(here::here("R", "ico_name.R"))
+source(here::here("R", "group_exemption_number.R"))
+source(here::here("R", "affiliation_code.R"))
+source(here::here("R", "ruling_date.R"))
 
 # Extraction Layer
 
@@ -28,33 +35,46 @@ data.table::fwrite(bmf_2025_raw, "data/raw/bmf_2025.csv")
 # Transformation Layer
 
 # EIN
-bmf_2025_preprocessed <- bmf_2025_raw |>
-  dplyr::mutate(ein = clean_and_format_ein(EIN))
+bmf_2025_preprocessed <- bmf_2025_raw[, ein := clean_and_format_ein(EIN)]
 
 ## NAME
-raw_names <- bmf_2025_raw$NAME
+raw_names <- bmf_2025_preprocessed$NAME
 
-bmf_2025_raw[, c("org_name_raw",
-                 "org_name_join",
-                 "org_name_display",
-                 "org_legal_suffix") := as.list(clean_names(raw_names, 
-                                                            suffix_map, 
-                                                            standardization_lookup, 
-                                                            name_lookup))]
+bmf_2025_preprocessed[, c("org_name_raw",
+                          "org_name_join",
+                          "org_name_display",
+                          "org_legal_suffix") := as.list(clean_names(raw_names, suffix_map, standardization_lookup, name_lookup))]
 
 
 ## ICO Name
-bmf_2025_raw <- transform_ico_name(bmf_2025_raw)
+bmf_2025_preprocessed <- transform_ico_name(bmf_2025_preprocessed)
 
 ## GEN
 
-bmf_2025_raw <- transform_group_exemption_number(bmf_2025_raw)
+bmf_2025_preprocessed <- transform_group_exemption_number(bmf_2025_preprocessed)
 
 ## EO SUBSECTION AND CLASSIFICATION CODES
+
+cl_code_dim_table <- create_cl_code_dim_table(bmf_2025_preprocessed, classification_code_lookup, "2025")
+
+bmf_2025_preprocessed <- transform_subsection_classification_codes(
+  bmf_2025_preprocessed,
+  cl_code_dim_table,
+  subsection_orgtype_lookup
+)
+
+## AFFILIATION CODE
+
+bmf_2025_preprocessed <- transform_affiliation_code(bmf_2025_preprocessed, 
+                                                    affiliation_code_lookup)
+
+## Ruling date
+bmf_2025_preprocessed <- transform_ruling_date(bmf_2025_preprocessed)
 
 # TODO
 # create copies, don't overwrite, make functions pure, preserve data lineage it must be clear that we are making changes outside the function
 # Merge raw bmf path vector and raw bmf urls together
 # complement names with efile names, since NAME is incorrect.
+# move to duckdb
 # Metadata tables:
 # organization name, ico name
