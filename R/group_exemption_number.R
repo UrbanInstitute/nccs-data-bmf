94f19d04eabfce7cd6246adcfc3069884bf13ae5
Thiyaghessan <thiyaghessan@gmail.com>
2025-10-29 17:47:42 -0400

    debug incorrect data.table syntax

--- R/group_exemption_number.R
+++ R/group_exemption_number.R
@@ -3,8 +3,8 @@
 transform_group_exemption_number  <- function(bmf_dt){
   dt <- data.table::copy(bmf_dt)
   data.table::setnames(dt, "GROUP", "group_exemption_number_raw")
-  dt[group_exemption_number_clean := stringr::str_pad(group_exemption_number_raw, width = 4, side = "left", pad = "0")]
-  dt[group_exemption_number_is_absent:= data.table::fifelse(
+  dt[, group_exemption_number_clean := stringr::str_pad(group_exemption_number_raw, width = 4, side = "left", pad = "0")]
+  dt[, group_exemption_number_is_absent:= data.table::fifelse(
     group_exemption_number_raw == "0",
     TRUE,
     FALSE
