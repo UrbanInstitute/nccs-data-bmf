53b00264ecc6d80ee7d366ea5586f12e9db704ce
Thiyaghessan <thiyaghessan@gmail.com>
2025-11-04 15:55:16 -0500

    Add additional lookup paths. Pivoting to single lookup .xlsx workbook stored in a list with each sheet named according to its contents for simplicity and interpretability

--- R/config.R
+++ R/config.R
@@ -19,3 +19,14 @@ subsection_classification_code_lookup_path <- "data/lookup/classification_subsec
 affiliation_code_lookup_path <- "data/lookup/affiliation_code_lookup.csv"
 deductibility_code_lookup_path <- "data/lookup/deductibility_code_lookup.csv"
 foundation_code_lookup_path <- "data/lookup/foundation_code_lookup.csv"
+activity_code_lookup_path <- "data/lookup/activity_code_lookup.csv"
+organization_code_lookup_path <- "data/lookup/organization_code_lookup.csv"
+
+# Lookup
+lookup_path <- "data/lookup/bmf_code_lookup.xlsx"
+lookup_ls <- openxlsx::getSheetNames(lookup_path) |>
+  purrr::set_names() |>
+  purrr::map(~ {
+    df <- openxlsx::read.xlsx(xlsxFile = lookup_path, sheet = .x)
+    data.table::setDT(df)
+  })
